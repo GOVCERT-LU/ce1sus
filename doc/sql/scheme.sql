5a2f2106fc3650025b335b404dbc057e277a3c92
Jean-Paul Weber <jean-paul.weber@govcert.etat.lu>
2013-10-31 16:27:31 +0100

    missing field in groups

--- doc/sql/scheme.sql
+++ doc/sql/scheme.sql
@@ -256,6 +256,7 @@ CREATE TABLE `Groups` (
   `auto_share_tlp` int(1) DEFAULT '0',
   `description` mediumtext,
   `canDownlad` int(1) DEFAULT '0',
+  `tlplvl` int(1) DEFAULT NULL,
   PRIMARY KEY (`group_id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
