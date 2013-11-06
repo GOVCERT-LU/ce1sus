7c3a9066bcb8d913e4f5ccc9afad8d62c06d2490
Jean-Paul Weber <jean-paul.weber@govcert.etat.lu>
2013-11-06 14:45:25 +0100

    Missing files for the permissions

---
+++ ce1sus/brokers/permission/subgroupbroker.py
@@ -0,0 +1,134 @@
+# -*- coding: utf-8 -*-
+
+"""This module provides container classes and interfaces
+for inserting data into the database.
+
+Created on Jul 4, 2013
+"""
+
+__author__ = 'Weber Jean-Paul'
+__email__ = 'jean-paul.weber@govcert.etat.lu'
+__copyright__ = 'Copyright 2013, GOVCERT Luxembourg'
+__license__ = 'GPL v3+'
+
+from dagr.db.broker import BrokerBase, NothingFoundException
+import sqlalchemy.orm.exc
+from dagr.db.broker import BrokerException
+from dagr.helpers.converters import ObjectConverter
+from ce1sus.brokers.permission.permissionclasses import Group, SubGroup
+
+
+class SubGroupBroker(BrokerBase):
+  """This is the interface between python an the database"""
+
+  def getBrokerClass(self):
+    """
+    overrides BrokerBase.getBrokerClass
+    """
+    return SubGroup
+
+  def getGroupsBySubGroup(self, identifier, belongIn=True):
+    """
+    get all users in a given group
+
+    :param identifier: identifier of the group
+    :type identifier: Integer
+    :param belongIn: If set returns all the attributes of the object else
+                     all the attributes not belonging to the object
+    :type belongIn: Boolean
+
+    :returns: list of Users
+    """
+    try:
+      groups = self.session.query(Group).join(SubGroup.groups).filter(
+                                        SubGroup.identifier == identifier).all()
+      if not belongIn:
+        groupIDs = list()
+        for subgroup in groups:
+          groupIDs.append(subgroup.identifier)
+        groups = self.session.query(Group).filter(~Group.identifier.in_(
+                                                                  groupIDs
+                                                                            )
+                                                        )
+      return groups
+    except sqlalchemy.orm.exc.NoResultFound:
+      users = list()
+    except sqlalchemy.exc.SQLAlchemyError as e:
+      self.getLogger().fatal(e)
+      self.session.rollback()
+      raise BrokerException(e)
+
+  def addGroupToSubGroup(self, groupID, subGroupID, commit=True):
+    """
+    Add a user to a group
+
+    :param userID: Identifier of the user
+    :type userID: Integer
+    :param groupID: Identifier of the group
+    :type groupID: Integer
+    """
+    try:
+      group = self.session.query(Group).filter(Group.identifier ==
+                                               groupID).one()
+      subgroup = self.session.query(SubGroup).filter(SubGroup.identifier
+                                                     == subGroupID).one()
+      subgroup.groups.append(group)
+      self.doCommit(commit)
+    except sqlalchemy.orm.exc.NoResultFound:
+      raise NothingFoundException('Group or subgroup not found')
+    except sqlalchemy.exc.SQLAlchemyError as e:
+      self.getLogger().fatal(e)
+      self.session.rollback()
+      raise BrokerException(e)
+
+  def removeSubGroupFromGroup(self, groupID, subGroupID, commit=True):
+    """
+    Removes a user to a group
+
+    :param userID: Identifier of the user
+    :type userID: Integer
+    :param groupID: Identifier of the group
+    :type groupID: Integer
+    """
+    try:
+      group = self.session.query(Group).filter(Group.identifier ==
+                                               groupID).one()
+      subgroup = self.session.query(SubGroup).filter(SubGroup.identifier
+                                                     == subGroupID).one()
+      subgroup.groups.remove(group)
+      self.doCommit(commit)
+    except sqlalchemy.orm.exc.NoResultFound:
+      raise NothingFoundException('Group or subgroup not found')
+    except sqlalchemy.exc.SQLAlchemyError as e:
+      self.getLogger().fatal(e)
+      self.session.rollback()
+      raise BrokerException(e)
+
+  # pylint: disable=R0903,R0913
+  @staticmethod
+  def buildSubGroup(identifier=None,
+                 name=None,
+                 description=None,
+                 action='insert'):
+    """
+    puts a subgroup with the data together
+
+    :param identifier: The identifier of the subgroup,
+                       is only used in case the action is edit or remove
+    :type identifier: Integer
+    :param name: The name of the subgroup
+    :type name: String
+    :param description: The description of this subgroup
+    :type description: String
+    :param action: action which is taken (i.e. edit, insert, remove)
+    :type action: String
+
+    :returns: Group
+    """
+    subgroup = SubGroup()
+    if not action == 'insert':
+      subgroup.identifier = identifier
+    if not action == 'remove':
+      subgroup.name = name.strip()
+      subgroup.description = description.strip()
+    return subgroup
---
+++ ce1sus/web/controllers/admin/subgroups.py
@@ -0,0 +1,212 @@
+# -*- coding: utf-8 -*-
+
+"""
+module handing the groups pages
+
+Created: Nov 5, 2013
+"""
+
+__author__ = 'Weber Jean-Paul'
+__email__ = 'jean-paul.weber@govcert.etat.lu'
+__copyright__ = 'Copyright 2013, GOVCERT Luxembourg'
+__license__ = 'GPL v3+'
+
+from ce1sus.web.controllers.base import Ce1susBaseController
+import cherrypy
+from ce1sus.web.helpers.protection import require, privileged, requireReferer
+from dagr.db.broker import OperationException, BrokerException, \
+  ValidationException, NothingFoundException
+import types as types
+from ce1sus.brokers.staticbroker import TLPLevel
+from ce1sus.brokers.permission.userbroker import UserBroker
+from ce1sus.brokers.permission.groupbroker import GroupBroker
+from ce1sus.brokers.permission.subgroupbroker import SubGroupBroker
+
+
+class SubGroupController(Ce1susBaseController):
+
+  def __init__(self):
+    Ce1susBaseController.__init__(self)
+    self.subgroupBroker = self.brokerFactory(SubGroupBroker)
+
+  @require(privileged(), requireReferer(('/internal')))
+  @cherrypy.expose
+  def index(self):
+    """
+    renders the group page
+
+    :returns: generated HTML
+    """
+
+    template = self.getTemplate('/admin/subgroups/subgroupBase.html')
+    return template.render()
+
+
+  @require(privileged(), requireReferer(('/internal')))
+  @cherrypy.expose
+  def leftContent(self):
+    """
+    renders the left content of the group index page
+
+    :returns: generated HTML
+    """
+    template = self.getTemplate('/admin/subgroups/subgroupLeft.html')
+    groups = self.subgroupBroker.getAll()
+    return template.render(groups=groups)
+
+  @require(privileged(), requireReferer(('/internal')))
+  @cherrypy.expose
+  def rightContent(self, subGroupid=0, group=None):
+    """
+    renders the right content of the group index page
+
+    :param subGroupid: The group id of the desired displayed group
+    :type subGroupid: Integer
+    :param group: Similar to the previous attribute but prevents
+                      additional loadings
+    :type group: Group
+
+    :returns: generated HTML
+    """
+    template = self.getTemplate('/admin/subgroups/subgroupRight.html')
+    if group is None:
+      try:
+        group = self.subgroupBroker.getByID(subGroupid)
+      except NothingFoundException:
+        group = None
+    else:
+      group = group
+    remGroups = None
+    groups = None
+    if not group is None:
+      remGroups = self.subgroupBroker.getGroupsBySubGroup(group.identifier,
+                                                             False)
+      groups = group.groups
+    return template.render(subgroupDetails=group,
+                           remainingGroups=remGroups,
+                           groups=groups,
+                           cbTLPValues=TLPLevel.getDefinitions())
+
+  @require(privileged(), requireReferer(('/internal')))
+  @cherrypy.expose
+  def addSubGroup(self):
+    """
+    renders the add a group page
+
+    :returns: generated HTML
+    """
+    template = self.getTemplate('/admin/subgroups/subgroupModal.html')
+    return template.render(group=None, errorMsg=None,
+                           cbTLPValues=TLPLevel.getDefinitions())
+
+  # pylint: disable=R0913
+  @require(privileged(), requireReferer(('/internal')))
+  @cherrypy.expose
+  def modifySubGroup(self, identifier=None, name=None,
+                  description=None, action='insert'):
+    """
+    modifies or inserts a group with the data of the post
+
+    :param identifier: The identifier of the group,
+                       is only used in case the action is edit or remove
+    :type identifier: Integer
+    :param name: The name of the group
+    :type name: String
+    :param description: The description of this group
+    :type description: String
+    :param action: action which is taken (i.e. edit, insert, remove)
+    :type action: String
+
+    :returns: generated HTML
+    """
+    template = self.getTemplate('/admin/groups/groupModal.html')
+    group = self.subgroupBroker.buildSubGroup(identifier,
+                                        name,
+                                        description,
+                                        action)
+    try:
+      if action == 'insert':
+        self.subgroupBroker.insert(group)
+      if action == 'update':
+        self.subgroupBroker.update(group)
+      if action == 'remove':
+        self.subgroupBroker.removeByID(group.identifier)
+      return self.returnAjaxOK()
+    except OperationException as e:
+      self.getLogger().info('OperationError occurred: {0}'.format(e))
+      return 'Cannot delete this group. The group is still referenced.'
+    except ValidationException:
+      self.getLogger().debug('Group is invalid')
+      return self.returnAjaxPostError() + template.render(group=group,
+                           cbTLPValues=TLPLevel.getDefinitions())
+    except BrokerException as e:
+      self.getLogger().error('An unexpected error occurred: {0}'.format(e))
+      return "Error {0}".format(e)
+
+  @require(privileged(), requireReferer(('/internal')))
+  @cherrypy.expose
+  def editSubGroup(self, groupid):
+    """
+    renders the edit group page
+
+    :param groupid: The group id of the desired displayed group
+    :type groupid: Integer
+
+    :returns: generated HTML
+    """
+    template = self.getTemplate('/admin/subgroups/subgroupModal.html')
+    errorMsg = None
+    try:
+      group = self.subgroupBroker.getByID(groupid)
+    except BrokerException as e:
+      self.getLogger().error('An unexpected error occurred: {0}'.format(e))
+      errorMsg = 'An unexpected error occurred: {0}'.format(e)
+    return template.render(group=group, errorMsg=errorMsg,
+                           cbTLPValues=TLPLevel.getDefinitions())
+
+  @require(privileged(), requireReferer(('/internal')))
+  @cherrypy.expose
+  def editSubGroupGroup(self, subGroupID, operation,
+                     subGroupGroups=None, remainingGroups=None):
+    """
+    modifies the relation between a group and its users
+
+    :param subGroupID: The subGroupID of the group
+    :type subGroupID: Integer
+    :param operation: the operation used in the context (either add or remove)
+    :type operation: String
+    :param remainingUsers: The identifiers of the users which the group is not
+                            attributed to
+    :type remainingUsers: Integer array
+    :param groupUsers: The identifiers of the users which the group is
+                       attributed to
+    :type groupUsers: Integer array
+
+    :returns: generated HTML
+    """
+    try:
+      if operation == 'add':
+        if not (remainingGroups is None):
+          if isinstance(remainingGroups, types.StringTypes):
+            self.subgroupBroker.addGroupToSubGroup(remainingGroups,
+                                                   subGroupID)
+          else:
+            for groupID in remainingGroups:
+              self.subgroupBroker.addGroupToSubGroup(remainingGroups,
+                                                     groupID,
+                                                     False)
+            self.subgroupBroker.doCommit(True)
+      else:
+        if not (subGroupGroups is None):
+          if isinstance(subGroupGroups, types.StringTypes):
+            self.subgroupBroker.removeSubGroupFromGroup(subGroupGroups,
+                                                        subGroupID)
+          else:
+            for groupID in subGroupGroups:
+              self.subgroupBroker.removeSubGroupFromGroup(subGroupGroups,
+                                                       groupID,
+                                                       False)
+            self.subgroupBroker.doCommit(True)
+      return self.returnAjaxOK()
+    except BrokerException as e:
+      return "Error {0}".format(e)
