7cf95ddc2f660cfe6cf083c7c2ea5e34f0cc1888
Jean-Paul Weber <jean-paul.weber@govcert.etat.lu>
2013-12-31 18:58:32 +0100

    removed ealier commit

--- test/testsuites.py
+++ test/testsuites.py
@@ -16,7 +16,7 @@ from test.ce1sus.brokers.definitionbroker import TestDefinitionBrokers
 from test.ce1sus.brokers.permissionbroker import TestPermissionBrokers
 from test.ce1sus.brokers.eventbroker import TestEventBrokers
 from test.ce1sus.helpers.bitdecoder import TestBitDecoder
-from test.ce1sus.api.ce1susapi import TestAPI
+
 
 
 def suite():
@@ -34,7 +34,7 @@ def suite():
   testSuite.addTest(TestConfiguration())
   testSuite.addTest(TestLog())
   testSuite.addTest(TestConverter())
-  testSuite.addTest(TestAPI())
+
 
   return testSuite
 
