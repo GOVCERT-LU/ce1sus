c073817b0e0e32892b1368b20c9f32ccf6a0163f
Jean-Paul Weber <jean-paul.weber@govcert.etat.lu>
2014-05-14 16:55:17 +0200

    Fixed sql

--- doc/sql/update_to_REL_0.8.16.sql
+++ doc/sql/update_to_REL_0.8.16.sql
@@ -1,4 +1,4 @@
 update Objects set parentEvent = event_id where parentEvent is Null;
 ALTER TABLE Objects DROP FOREIGN KEY `FK_objects_events_event_id` ;
 ALTER TABLE Objects DROP COLUMN `event_id`, DROP INDEX `IDX_objects_events_event_id` ;
-UPDATE `ce1sus`.`ce1sus` SET `value`='0.8.16' WHERE `ce1sus_id`='1';
+UPDATE ce1sus SET `value`='0.8.16' WHERE `ce1sus_id`='1';
