e66f50e18cff1281c661cbd8a2dc62ea0812d876
Jean-Paul Weber <jean-paul.weber@govcert.etat.lu>
2015-03-18 09:08:45 +0100

    Missed files

---
+++ ce1sus/views/web/api/version3/handlers/admin/adminindicatorhandler.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+
+"""
+(Description)
+
+Created on Mar 16, 2015
+"""
+from ce1sus.controllers.events.indicatorcontroller import IndicatorController
+from ce1sus.views.web.api.version3.handlers.restbase import RestBaseHandler, rest_method, methods, require
+
+
+__author__ = 'Weber Jean-Paul'
+__email__ = 'jean-paul.weber@govcert.etat.lu'
+__copyright__ = 'Copyright 2013-2014, GOVCERT Luxembourg'
+__license__ = 'GPL v3+'
+
+
+class AdminIndicatorTypesHandler(RestBaseHandler):
+
+  def __init__(self, config):
+    RestBaseHandler.__init__(self, config)
+    self.indicator_controller = IndicatorController(config)
+
+  @rest_method(default=True)
+  @methods(allowed=['GET'])
+  @require()
+  def default(self, **args):
+    values = self.indicator_controller.get_all_types()
+    result = list()
+    details = self.get_detail_value(args)
+    for value in values:
+      result.append(value.to_dict(details))
+    return result
